27fbafbb09007eabad71af4bf971bda62614e12b
Hoàng Đỗ TOàn <hoangdotoan3112002@gmail.com>
2025-01-11 11:23:06 +0700

    Text Input

--- App.tsx
+++ App.tsx
@@ -1,28 +1,44 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-const [name, setName] = useState<string>("e cu, "); 
-const [count, setCount] = useState<number>(0);
-const [test, setTest] = useState<boolean>(false);
-const [test1, setTest1] = useState(
-  {
+  const [count, setCount] = useState<number>(0);
+  const [test, setTest] = useState<boolean>(false);
+  const [test1, setTest1] = useState({
     name: "e cu",
     age: 25,
-  }
-)
+  });
+
+  const [inputString, setInputString] = useState<string>("");
+  const [inputNumber, setInputNumber] = useState<number>(0);
   return (
     <View style={styles.container}>
-      <Text style={styles.hello}>
-        {count}
-      </Text>
+      <View>
+        <Text style={{ fontSize: 30, fontWeight: "600" }}>Name: {inputString}</Text>
+        <TextInput
+          multiline
+          style={styles.textinput}
+          onChangeText={(value) => setInputString(value)}
+        />
+      </View>
+
+      <View>
+        <Text style={{ fontSize: 30, fontWeight: "600" }}>Age: {inputNumber}</Text>
+        <TextInput
+          multiline
+          style={styles.textinput}
+          onChangeText={(value) => setInputNumber(+value)}
+          keyboardType="numeric"
+        />
+      </View>
+      <Text style={styles.hello}>{count} times</Text>
 
       <View>
         <Button
           title="Increase"
           onPress={() => {
-            setCount(count + 1)
+            setCount(count + 1);
           }}
         />
       </View>
@@ -38,8 +54,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  hello:{
+  hello: {
     fontSize: 50,
-    fontWeight: "600"
-  }
+    fontWeight: "600",
+  },
+  textinput: {
+    borderColor: "green",
+    borderWidth: 1,
+    width: 200,
+    padding: 15,
+  },
 });
