97f4ee420a6fff63a5bed931f8b7858c7af12aed
Hoàng Đỗ TOàn <hoangdotoan3112002@gmail.com>
2025-01-13 06:08:42 +0700

    Using Array

--- App.tsx
+++ App.tsx
@@ -1,67 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState<number>(0);
-  const [test, setTest] = useState<boolean>(false);
-  const [test1, setTest1] = useState({
-    name: "e cu",
-    age: 25,
-  });
+  const [student, setStudent] = useState([
+    { id: 1, name: "Toan", age: 22 },
+    { id: 2, name: "Toan1", age: 18 },
+    { id: 3, name: "Toan2", age: 20 },
+    { id: 4, name: "Toan3", age: 21 },
+    { id: 5, name: "Toan4", age: 24 },
+    { id: 6, name: "Toan5", age: 19 },
+    { id: 7, name: "Toan6", age: 18 },
+    { id: 8, name: "Toan7", age: 20 },
+    { id: 9, name: "Toan8", age: 22 },
+  ]);
 
-  const [inputString, setInputString] = useState<string>("");
-  const [inputNumber, setInputNumber] = useState<number>(0);
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={{ fontSize: 30, fontWeight: "600" }}>Name: {inputString}</Text>
-        <TextInput
-          multiline
-          style={styles.textinput}
-          onChangeText={(value) => setInputString(value)}
-        />
-      </View>
-
-      <View>
-        <Text style={{ fontSize: 30, fontWeight: "600" }}>Age: {inputNumber}</Text>
-        <TextInput
-          multiline
-          style={styles.textinput}
-          onChangeText={(value) => setInputNumber(+value)}
-          keyboardType="numeric"
-        />
-      </View>
-      <Text style={styles.hello}>{count} times</Text>
-
-      <View>
-        <Button
-          title="Increase"
-          onPress={() => {
-            setCount(count + 1);
-          }}
-        />
-      </View>
+      <Text style={styles.text}>Hello World</Text>
+      <ScrollView>
+        {student.map(item => {
+          return (
+            <View key={item.id} style={styles.item}>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-//css in js
+// CSS in JS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 50,
+    paddingHorizontal: 30,
   },
-  hello: {
-    fontSize: 50,
+  text: {
+    fontSize: 30,
     fontWeight: "600",
   },
-  textinput: {
-    borderColor: "green",
-    borderWidth: 1,
-    width: 200,
-    padding: 15,
+  item: {
+    backgroundColor: "pink",
+    padding: 30,
+    marginBottom: 30,
   },
 });
